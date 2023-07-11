7820e01877ce15fc09e1707cca059b44f2f31503
Nahuel Rodriguez <nahufed@gmail.com>
2023-07-11 12:49:49 -0300

    agregamos middlewares cors para conexiones remotas y express para archivos json

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 
 app.set('port',  process.env.PORT || 4000);
 app.listen(app.get('port'),()=>{
-    console.log('Estoy en el puerto' +app.get('port'))
+    console.log('Estoy en el puerto ' +app.get('port'))
 });
+
+
+app.use(cors());
+app.use(express.json());
