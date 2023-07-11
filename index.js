d9bca7386d48767b5f808b3f69958c7bfafa8062
Nahuel Rodriguez <nahufed@gmail.com>
2023-07-11 12:48:48 -0300

    agregamos un puerto en la configuracion del index

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 import express from 'express';
-console.log("que onda")
+const app = express();
+
+app.set('port',  process.env.PORT || 4000);
+app.listen(app.get('port'),()=>{
+    console.log('Estoy en el puerto' +app.get('port'))
+});
